12407a69fb01d4056f4a16b856fc0dce9366621a
Musa Ahmetcan Türker <ahmetcantrker@yahoo.com>
2017-11-02 22:57:04 +0300

    a

---
+++ work/game/Rule The Nation/Assets/Scripts/CameraRotationScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CameraRotationScript : MonoBehaviour {
+    private bool started = false;
+    
+    public Camera targetCamera;
+    public float targetRotationY;
+    public float stepSize = 0.05f;
+
+    private void Update()
+    {
+        var difference = Mathf.Abs(targetCamera.transform.rotation.eulerAngles.y - targetRotationY);
+        
+        if (started)
+        {
+            if (difference <= stepSize)
+            {
+                targetCamera.transform.rotation = Quaternion.Euler(
+                    targetCamera.transform.rotation.eulerAngles.x,
+                    targetRotationY,
+                    targetCamera.transform.rotation.eulerAngles.z);
+                started = false;
+            }
+            else
+                targetCamera.transform.rotation = Quaternion.Euler(
+                    targetCamera.transform.rotation.eulerAngles.x,
+                    targetCamera.transform.rotation.eulerAngles.y + stepSize,
+                    targetCamera.transform.rotation.eulerAngles.z);
+        }
+    }
+
+    public void Rotate()
+    {
+        started = true;
+    }
+}
